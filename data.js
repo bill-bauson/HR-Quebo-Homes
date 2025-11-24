9a146fcafad12bb020bba3f96d164195c3f9bed2
bill-bauson <124177918+bill-bauson@users.noreply.github.com>
2025-11-24 17:51:50 +0000

    Initial commit

---
+++ data.js
@@ -0,0 +1,55 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lacy-and-schafer-homes",
+      "name": "Lacy and Schafer homes",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.015911593142448055,
+          "pitch": -0.3523186545053818,
+          "title": "Lacy home",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.668553730004323,
+          "pitch": -0.4172685263843565,
+          "title": "Schafer home",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Hope Remembered Quebo Homes",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
